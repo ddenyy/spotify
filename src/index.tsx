280cb9410eb503a1cfae33a1f739009ccb246e7e
Denis Osipov <ddennios@mail.ru>
2022-03-21 17:44:56 +0300

    Диплом. Верстка

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 
@@ -9,3 +9,5 @@ import './index.css';
 //   </React.StrictMode>,
 //   document.getElementById('root')
 // );
+
+
